76703d255b0b0de65172a5d1f6a3872b6acfb5fd
jigar-patel-sf <jigar.patel@securefact.com>
2018-04-25 11:40:23 -0400

    Completed the basics

--- ShoppingListRecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingListRecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+  new Recipe("Test Recipe 1","Test Description 1","https://www.mamador.com.ng/media/catalog/product/cache/76/thumbnail/9df78eab33525d08d6e5fb8d27136e95/s/a/samosa_small.1477920885.jpg"),
+  new Recipe("Test Recipe 2","Test Description 2","https://www.mamador.com.ng/media/catalog/product/cache/76/thumbnail/9df78eab33525d08d6e5fb8d27136e95/s/a/samosa_small.1477920885.jpg")
+];
   constructor() { }
 
   ngOnInit() {
